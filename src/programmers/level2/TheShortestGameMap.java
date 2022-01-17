a00560815fab9d403e256f9c1d518019868fcb0e
kyutae <k305059ute@gmail.com>
2022-01-17 19:56:07 +0900

    코테 공부

--- src/programmers/level2/TheShortestGameMap.java
+++ src/programmers/level2/TheShortestGameMap.java
@@ -11,15 +11,41 @@ public class TheShortestGameMap {
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
     static boolean[][] visited;
+    static int min = Integer.MAX_VALUE;
 
     public static int solution(int[][] maps) {
         visited = new boolean[maps.length][maps[0].length];
-        return bfs(0,0,maps);
+        dfs(0, 0, maps, 0);
+        if (min == Integer.MAX_VALUE)
+            return -1;
+        return min;
+//        return bfs(0,0,maps);
+    }
+
+    public static void dfs(int x, int y, int[][] maps, int cnt) {
+
+        cnt++;
+        if (x == maps.length - 1 && y == maps[0].length - 1) {
+            min = Math.min(cnt, min);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+            if (nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length)
+                continue;
+            if (maps[nx][ny] == 1 && !visited[nx][ny]) {
+                visited[nx][ny] = true;
+                dfs(nx, ny, maps, cnt);
+                visited[nx][ny] = false;
+            }
+        }
     }
 
     public static int bfs(int x, int y, int[][] maps) {
         Queue<Node> q = new LinkedList<>();
-        q.add(new Node(x, y, 1));
+        q.offer(new Node(x, y, 1));
         visited[x][y] = true;
 
         while (!q.isEmpty()) {
@@ -27,17 +53,16 @@ public class TheShortestGameMap {
             if (node.x == maps.length - 1 && node.y == maps[0].length - 1)
                 return node.cnt;
             for (int i = 0; i < 4; i++) {
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
+                int nx = dx[i] + node.x;
+                int ny = dy[i] + node.y;
                 if (nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length)
                     continue;
                 if (maps[nx][ny] == 1 && !visited[nx][ny]) {
                     visited[nx][ny] = true;
-                    q.add(new Node(nx, ny, node.cnt + 1));
+                    q.offer(new Node(nx, ny, node.cnt + 1));
                 }
             }
         }
-
         return -1;
     }
 
@@ -45,6 +70,7 @@ public class TheShortestGameMap {
         int x;
         int y;
         int cnt;
+
         public Node(int x, int y, int cnt) {
             this.x = x;
             this.y = y;
