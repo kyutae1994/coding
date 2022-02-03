2dbcf6b8faf3cb103c713342583975c9014ee833
kyutae1994 <k305059ute@gmail.com>
2022-02-03 14:22:33 +0900

    코테 공부

--- src/programmers/level2/CountingTheNumberOfQuadsAfterCompressed.java
+++ src/programmers/level2/CountingTheNumberOfQuadsAfterCompressed.java
@@ -2,7 +2,7 @@ package programmers.level2;
 
 import java.util.Arrays;
 
-public class CountingTheNumberOfQuadsAfterCompressed {
+public class CountingTheNumberOfQuadsAfterCompressed { // 3페이지 & 쿼드압축 후 개수 세기
     public static void main(String[] args) {
         int[][] arr = {{1,1,0,0},{1,0,0,0},{1,0,0,1},{1,1,1,1}};
         System.out.println(Arrays.toString(solution(arr)));
