7ad037950f04ce043627b6610ae50a16f32c16b7
kyutae1994 <k305059ute@gmail.com>
2022-02-11 14:56:46 +0900

    코테 공부

---
+++ src/programmers/level2/minAndMax.java
@@ -0,0 +1,19 @@
+package programmers.level2;
+
+import java.util.*;
+
+public class minAndMax {
+    public static void main(String[] args) {
+        System.out.println(solution("-1 -2 -3 -4"));
+    }
+    public static String solution(String s) {
+        String[] arr = s.split(" ");
+        int[] num = Arrays.stream(arr).mapToInt(Integer :: parseInt).toArray();
+        long[] n = Arrays.stream(arr).mapToLong(Long :: parseLong).toArray();
+        System.out.println(Arrays.toString(n));
+        Arrays.sort(num);
+        int min = num[0];
+        int max = num[arr.length - 1];
+        return min + " " + max;
+    }
+}
