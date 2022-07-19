af2ceb4dba01e6b84bb1abcc839aee6fc62101aa
kyutae <k305059ute@gmail.com>
2022-07-19 17:41:52 +0900

    코테 공부

---
+++ src/programmers/level3/left/page1/NetWork_5.java
@@ -0,0 +1,34 @@
+package programmers.level3.left.page1;
+
+public class NetWork_5 {
+    public static void main(String[] args) {
+        int[][] computers = {{1,1,0},{1,1,0},{0,0,1}};
+        System.out.println(solution(3, computers));
+    }
+
+    static boolean[] visited;
+
+    public static int solution(int n, int[][] computers) {
+        visited = new boolean[n];
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited = dfs(i, computers);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static boolean[] dfs(int i, int[][] computers) {
+        visited[i] = true;
+        for (int j = 0; j < visited.length; j++) {
+            if (i != j && !visited[j] && computers[i][j] == 1) {
+                dfs(j, computers);
+            }
+        }
+
+        return visited;
+    }
+}
